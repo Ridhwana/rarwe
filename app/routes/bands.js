ac8ee70515369a6c61fa111f6fff189ab0c2413a
Ridhwana <ridhwana.khan16@gmail.com>
2015-06-08 13:06:31 +0200

    feat: Routing - defining the underlying model for the bands and songs

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
+var Band = Ember.Object.extend({
+  name: ''
+});
+
+var ledZappelin = Band.create({
+  name: 'ledZappelin'
+});
+
+var pearlJam = Band.create({
+  name: 'pearlJam'
+});
+
+var fooFighters = Band.create({
+  name: 'fooFighters'
+})
+var bands = [ledZappelin, pearlJam, fooFighters];
+
 export default Ember.Route.extend({
+  model: function() {
+    return bands;
+  }
 });
